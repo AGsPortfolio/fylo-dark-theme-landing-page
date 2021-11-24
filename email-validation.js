90894e536ce73f6f2ea926e161e5cfb8ca169afd
AGsPortfolio <64134099+AGsPortfolio@users.noreply.github.com>
2021-11-24 11:50:19 +0100

    Finishing touch

--- email-validation.js
+++ email-validation.js
@@ -3,6 +3,7 @@ function emailValidation (email) {
     var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(email.value.match(emailFormat)) {
         document.getElementById('emailMessage').style="visibility:hidden";
+        document.getElementById('thanks').style="visibility:visible";
         return true;
     }
     if(email.value == "") {
